b6a9a572f286d14d18f1ce18d32f3492a9f4c70f
AndreyShvachko <an.shvachko@gmail.com>
2024-10-16 17:27:47 +0300

    dynamic programming implemented

---
+++ task_02.py
@@ -0,0 +1,24 @@
+def find_min_coins(amount, coins=[50, 25, 10, 5, 2, 1]):
+
+    #таблиця для мінімільної кількості монет для кожної суми від 0 до amount
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0 # 0 монет для суми 0
+
+    #заповнення таблиці dp
+    for coin in coins:
+        for x in range(coin, amount +1):
+            if dp[x - coin] + 1 < dp[x]:
+                dp[x] = dp[x - coin] +1
+
+    #відновлення набору монет з таблиці dp
+    result = {}
+    current = amount
+    while current > 0:
+        for coin in coins:
+            if current >= coin and dp[current - coin] == dp[current] - 1:
+                result[coin] = result.get(coin, 0) +1
+                current -= coin
+                break
+    return result
+
+print(find_min_coins(113))
