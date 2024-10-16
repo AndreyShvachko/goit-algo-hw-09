67ae85c7df442ee1f40592fb894f68a4fa4ff39c
AndreyShvachko <an.shvachko@gmail.com>
2024-10-16 17:17:16 +0300

    greedy algorythm implemented

---
+++ task_01.py
@@ -0,0 +1,10 @@
+def find_coins_greedy(amount, coins=[50, 25, 10, 5, 2, 1]):
+    result = {}
+    for coin in coins:
+        count = amount // coin
+        if count > 0:
+            result[coin] = count
+        amount %= coin
+    return result
+
+print(find_coins_greedy(113))
